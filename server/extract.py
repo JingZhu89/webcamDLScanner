bea196c8278f8df54b5ede1f7d4d57bf5888eae8
Jing Zhu <jing.harrell@gmail.com>
2024-02-23 11:16:13 -0600

    switch backend from django to flask

---
+++ server/extract.py
@@ -0,0 +1,46 @@
+import easyocr
+import keras_ocr
+from abc import ABC, abstractmethod
+
+class OCR(ABC):
+  def __init__(self, path) -> None:
+    self.path = path
+
+  @abstractmethod
+  def extract(self):
+    pass
+
+class EasyOCR(OCR):
+  MIN = -10
+  MAX = 10
+  def __init__(self, path) -> None:
+    super().__init__(path)
+
+  def extract(self):
+    result = []
+    reader = easyocr.Reader(['en'], gpu = True)
+    extracted_info = reader.readtext(self.path, min_size = 1)
+    for tuple in extracted_info:
+      data = {'coordinate': tuple[0], 'text': tuple[1], 'confidence':tuple[2]}
+      result.append(data)
+    return result
+
+class KerasOCR(OCR):
+  MIN = -8
+  MAX = 10
+  def __init__(self, path) -> None:
+    super().__init__(path)
+
+  def extract(self):
+    result = []
+    pipeline = keras_ocr.pipeline.Pipeline()
+    extracted_info = pipeline.recognize([self.path])
+    for tuple in extracted_info[0]:
+      data = {'coordinate': tuple[1], 'text': tuple[0], 'confidence': None}
+      result.append(data)
+    return result
+
+
+
+
+
---
+++ server/preProcessor.py
@@ -0,0 +1,66 @@
+import cv2
+import numpy as np
+from deskew import determine_skew
+from scipy import ndimage
+import os
+BASE_PATH = 'processed_images/'
+class PreProcessor:
+  def __init__(self, path) -> None:
+    self.originalImgPath = path
+    self.imgName = self._extractFileName()
+    self.img = cv2.imread(path)
+    self._normalize()
+    self._skewCorrection()
+    self._noiseRemoval()
+    self._thinning()
+    self._grayScale()
+    self._thresholding()
+
+  def _normalize(self):
+    norm_img = np.zeros((self.img.shape[0], self.img.shape[1]))
+    self.img = cv2.normalize(self.img, norm_img, 0, 255, cv2.NORM_MINMAX)
+    newPath = BASE_PATH + self.imgName + '_normalized.jpg'
+    cv2.imwrite(newPath, self.img)
+    self.normalizedImgPath = newPath
+
+  def _skewCorrection(self):
+    grayscale = cv2.cvtColor(self.img, cv2.COLOR_BGR2GRAY)
+    angle = determine_skew(grayscale)
+    self.img = ndimage.rotate(self.img, angle)
+    newPath = BASE_PATH + self.imgName + '_skewCorrected.jpg'
+    cv2.imwrite(newPath, self.img)
+    self.skewCorrectedImgPath = newPath
+
+  def _noiseRemoval(self):
+    self.img = cv2.fastNlMeansDenoisingColored(self.img, None, 10, 10, 7, 15)
+    newPath = BASE_PATH + self.imgName + '_noiseRemoved.jpg'
+    cv2.imwrite(newPath, self.img)
+    self.noiseRemovedImgPath = newPath
+
+  def _thinning(self):
+    kernel = np.ones((2,2),np.uint8)
+    self.img = cv2.erode(self.img, kernel, iterations = 1)
+    newPath = BASE_PATH + self.imgName + '_eroded.jpg'
+    cv2.imwrite(newPath, self.img)
+    self.erodedImgPath = newPath
+
+  def _grayScale(self):
+    self.img = cv2.cvtColor(self.img, cv2.COLOR_BGR2GRAY)
+    newPath = BASE_PATH + self.imgName + '_grayScale.jpg'
+    cv2.imwrite(newPath, self.img)
+    self.grayScaleImgPath = newPath
+
+  def _thresholding(self):
+    self.img = cv2.threshold(self.img, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)[1]
+    newPath = BASE_PATH + self.imgName + '_threshold.jpg'
+    cv2.imwrite(newPath, self.img)
+    self.thresholdImgPath = newPath
+
+  def _extractFileName(self):
+    return os.path.basename(self.originalImgPath).split('/')[-1].split('.')[0]
+
+  def cleanupFiles(self):
+    my_dir = BASE_PATH
+    for fname in os.listdir(my_dir):
+      if self.imgName in fname:
+        os.remove(os.path.join(my_dir, fname))
